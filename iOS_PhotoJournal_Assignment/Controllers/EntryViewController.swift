178be92752f22d5534c9f7977657ebbfa76cb4ba
aglegaspi <lex83nyc@gmail.com>
2019-09-29 13:09:52 -0400

    reconfigured Collection View Cell

---
+++ iOS_PhotoJournal_Assignment/Controllers/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  iOS_PhotoJournal_Assignment
+//
+//  Created by Alexander George Legaspi on 9/29/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS_PhotoJournal_Assignment/Views/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  iOS_PhotoJournal_Assignment
+//
+//  Created by Alexander George Legaspi on 9/29/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var photoName: UILabel!
+    @IBOutlet weak var photoDate: UILabel!
+    
+    
+}
