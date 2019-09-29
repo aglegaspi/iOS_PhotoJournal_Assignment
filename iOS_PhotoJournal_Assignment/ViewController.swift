3894ac21b1a41bc4c2f074992ada82e8d38a3c7f
aglegaspi <lex83nyc@gmail.com>
2019-09-29 12:25:13 -0400

    first commit

---
+++ iOS_PhotoJournal_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_PhotoJournal_Assignment
+//
+//  Created by Alexander George Legaspi on 9/29/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
