fb1624694773e42823a289ad13fe3325f9ae9c73
aglegaspi <lex83nyc@gmail.com>
2019-10-02 20:59:46 -0400

    loading Entry View Controller from View Controller

--- iOS_PhotoJournal_Assignment/Controllers/EntryViewController.swift
+++ iOS_PhotoJournal_Assignment/Controllers/EntryViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image = UIImage() {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    @IBAction func photoLibraryPressed(_ sender: UIBarButtonItem) {
+        
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        present(imagePickerViewController,animated: true, completion: nil)
+    }
+    
 }
+
+extension EntryViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("\ninfo: \(info)\n") // keys: UIImagePickerController.InfoKey.originalImage
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            print("no image found")
+            return
+        }
+        self.image = image
+        dismiss(animated: true, completion: nil)
+    }
+}
+
--- iOS_PhotoJournal_Assignment/Controllers/ViewController.swift
+++ iOS_PhotoJournal_Assignment/Controllers/ViewController.swift
@@ -11,12 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func addImageButtonPress(_ sender: UIBarButtonItem) {
+        
+        //let favorite = favorites[indexPath.row]
+        let detailVC = storyboard?.instantiateViewController(withIdentifier: "AddNewPhoto") as! EntryViewController
+        //detailVC.photo = favorite
+        self.navigationController?.pushViewController(detailVC,animated: true)
+    }
+    
 }
 
